eb7b0f076f266d7ff90be4a492c0a964edae8c96
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-09 00:42:20 +0900

    feat: parse data

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
-use std::net::UdpSocket;
-use std::io::{Error, ErrorKind};
+use tokio::net::UdpSocket;
 
-fn main() -> std::io::Result<()> {
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
     {
-        let socket = UdpSocket::bind("127.0.0.1:3000")?;
+        let mut socket = UdpSocket::bind("127.0.0.1:3000").await?;
 
         loop {
             let mut buf = [0; 1024 * 64];
-            match socket.recv_from(&mut buf) {
+            match socket.recv_from(&mut buf).await {
                 Ok((amt, _)) => {
-                    let mut pos :usize = 0;
-                    loop {
-                        println!("{:}", buf[pos]);
-                        if pos >= amt {
-                            return Err(Error::new(ErrorKind::Other, "invalid data"));
+                    if amt < 3 {
+                        "invalid data";
+                    } else {
+                        let mut pos :usize = 1;
+                        loop {
+                            if pos >= amt - 2 {
+                                break;
+                            }
+                            if buf[pos] == 0x0d && buf[pos+1] == 0x0a {
+                                break;
+                            }
+                            pos += 1;
                         }
-                        if buf[pos] == 0x0d && buf[pos+1] == 0x0a {
-                            break;
-                        }
-                        pos += 1;
+                        let cmd = &buf[0];
+                        let key = &buf[1..pos];
+                        let value = &buf[pos + 2..amt];
+                        println!("cmd: {:?}, key: {:?}, val: {:?}", cmd, key, value);
                     }
-                    let key = &buf[0..pos];
-                    let value = &buf[pos + 2..amt-1];
-                    println!("key: {:?}, val: {:?}", key, value);
                 },
                 Err(e) =>
                     eprintln!("error: {}", e)
