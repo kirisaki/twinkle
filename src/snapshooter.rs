e4e6f282e225f1795d82d8d071f9b667da8b3bf6
Akihito KIRISAKI <kirisaki@klaraworks.net>
2019-12-17 16:38:11 +0900

    feat: snapshoot

--- src/snapshooter.rs
+++ src/snapshooter.rs
@@ -1,20 +1,23 @@
-use std::sync::Arc;
 use std::path::Path;
 use std::fs::File;
-use std::io;
 use std::time::Duration;
-use tokio::sync::Mutex;
+use std::thread::sleep;
 
-use crate::types::*;
 use crate::store::*;
 
 
 pub struct Snapshooter<P: AsRef<Path>> {
-    pub store: Arc<Mutex<Store>>,
+    pub store: Store,
     pub path: P,
     pub duration: Duration,
 }
 
 impl <P: AsRef<Path>> Snapshooter<P> {
-
+    pub async fn run(self) -> Result<(), std::io::Error>
+    {
+        let Snapshooter{store, path, duration} = self;
+        sleep(duration);
+        store.serialize(File::open(path)?).await?;
+        Ok(())
+    }
 }
